88d97734b0f73f084d44a7b20c25d368936363e8
Agent-grey <markkibuthu@gmail.com>
2024-07-26 00:50:43 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // Remove the main element
-    const main = document.getElementById('main');
-    if (main) {
-        main.remove();
-    }
+const dodger = document.getElementById("dodger");
+dodger.style.left = "180px"; 
 
-    // Create a new h1 element
-    const newHeader = document.createElement('h1');
-    newHeader.id = 'victory';
-    newHeader.innerHTML = 'YOUR-NAME is the champion'; // Replace YOUR-NAME with your actual name
-    document.body.appendChild(newHeader);
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+  const rightLimit = 360; 
+
+  if (left < rightLimit) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function(event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+  if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
 });
